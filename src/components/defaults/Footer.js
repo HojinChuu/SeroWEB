ee2302ddea88fc02d8c720871e079bdfdeb3dfb7
HojinChuu <cnghwls7@gmail.com>
2021-03-21 21:58:25 +0900

    footer design

--- src/components/defaults/Footer.js
+++ src/components/defaults/Footer.js
@@ -11,41 +11,29 @@ const Footer = () => {
       {location.pathname !== "/qrcode" && (
         <footer>
           <div className="row justify-content-center">
-            {width >= 1200 ? (
-              <div
-                className={
-                  location.pathname === "/" && width <= 414
-                    ? // ? "col col-8 p-5 mb-4"
-                      "col col-8 p-5"
-                    : "col col-8 p-5"
-                }
-              >
-                <div className="row justify-content-between title">
-                  <div className="col">
-                    Copyright &copy; 2021 Seropost.All right reserved
+            {width >= 1080 ? (
+              <div className="col col-12 pt-5 pb-5">
+                <div className="m-auto" style={{ width: "1047px" }}>
+                  <div className="row justify-content-between title">
+                    <div>Copyright &copy; 2021 Seropost.All right reserved</div>
+                    <div>
+                      <span>개인정보 처리방침</span>
+                      <span>이용약관</span>
+                      <span>고객센터</span>
+                    </div>
                   </div>
-                  <div>
-                    <span>개인정보 처리방침</span>
-                    <span>이용약관</span>
-                    <span>고객센터</span>
+                  <div className="row mt-4 subTitle justify-content-center">
+                    <span>개인정보책임관리자: 안준용</span>
+                    <span>사업자등록번호: 123-45-678910</span>
+                    <span>통신판매업신고번호: 제2021-대구북구-0443호</span>
+                    <span>
+                      주소: 대구광역시 북구 대학로 53 해피하우스 201호
+                    </span>
                   </div>
-                </div>
-                <div className="row mt-4 subTitle justify-content-around">
-                  <span>개인정보책임관리자: 안준용</span>
-                  <span>사업자등록번호: 123-45-678910</span>
-                  <span>통신판매업신고번호: 제2021-대구북구-0443호</span>
-                  <span>주소: 대구광역시 북구 대학로 53 해피하우스 201호</span>
                 </div>
               </div>
             ) : (
-              <div
-                className={
-                  location.pathname === "/" && width <= 414
-                    ? // ? "col p-3 mb-4"
-                      "col pt-2 pr-2 pl-2 "
-                    : "col pt-2 pr-2 pl-2"
-                }
-              >
+              <div className="col pt-2 pr-2 pl-2">
                 <div className="title" style={{ fontSize: "14px" }}>
                   <div className="ml-3">
                     Copyright &copy; 2021 Seropost.All right reserved
@@ -54,10 +42,10 @@ const Footer = () => {
                     <span style={{ fontSize: "14px", marginLeft: "17px" }}>
                       개인정보 처리방침
                     </span>
-                    <span style={{ fontSize: "14px", marginLeft: "0" }}>
+                    <span style={{ fontSize: "14px", marginLeft: "7px" }}>
                       이용약관
                     </span>
-                    <span style={{ fontSize: "14px", marginLeft: "0" }}>
+                    <span style={{ fontSize: "14px", marginLeft: "7px" }}>
                       고객센터
                     </span>
                   </div>
--- src/screens/MyPageScreen.js
+++ src/screens/MyPageScreen.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
-const MyPageScreen = () => {
+const MyPageScreen = ({ history }) => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (!userInfo && !localStorage.getItem("userToken")) {
+      history.push("/");
+    }
+  }, [userInfo, history]);
+
   return <h1 className="text-center">준비중 입니다...</h1>;
 };
 
